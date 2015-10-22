78128385d979e819724bb949cc62312d2b99d7b3
chantal517 <Chantal@mwc-020009.dhcp.missouri.edu>
2015-10-22 15:44:19 -0500

    files

---
+++ script.js
@@ -0,0 +1,8 @@
+SC.initialize({
+    client_id: '078f0fc585f0f320885eb514c564df8b',
+    redirect_uri: 'http://babbage.cs.missouri.edu/~cflzr3/soundcloud/callback.html'
+});
+
+//console.log("GEGFD");
+
+
